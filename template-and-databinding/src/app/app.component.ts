2378a8a656a691ffec01e2d9e533e00f19cdd48a
Pramod Para <ppara@ppara.local>
2017-08-20 20:20:18 +0530

    Displaying data examples

--- template-and-databinding/src/app/app.component.ts
+++ template-and-databinding/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero'; 
+// templateUrl or template can be used.
+// which ever comes last will be rendered in the browser.
+// For example in the below example if the templateUrl is followed by 
+// something like
+// template: `<h1> Helloworld </h1>` (template inline)
+// Hello world will be rendered and templateUrl will be neglected.
 
+
+// Angular creates an instance of the component when encountered in HTML 
+// as <app-root></app-root>
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  name: string;
+  heroes: string[];
+  // use typescript class to shape model data for the component.
+  heroesObj: Hero[];
+  show: boolean;
+  constructor() {
+    this.title = 'Data binding';
+    this.name = "Thor";
+    this.heroes = ['Ironman', 'Superman', 'Antman'];
+    this.heroesObj = [
+      new Hero(1,'A'),
+      new Hero(2,'B'),
+      new Hero(3,'C')
+    ]
+    this.show = true;
+  }
 }
